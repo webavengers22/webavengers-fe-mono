43d780ac4e333bc3e02833a5c3916f7f6a710795
짱유나 <yajung0203@naver.com>
2023-07-16 21:06:59 +0900

    chore : set eslint-config-custom

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,11 +1,30 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "turbo",
+    "prettier",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@next/next/recommended",
+  ],
   rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off", // 추후 warn 변경 필요
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
     "@next/next/no-html-link-for-pages": "off",
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": "off",
+    "react/button-has-type": "off",
+    "import/prefer-default-export": "off",
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
   },
-  parserOptions: {
-    babelOptions: {
-      presets: [require.resolve("next/babel")],
+  globals: {
+    React: "writable",
+  },
+  settings: {
+    next: {
+      rootDir: ["apps/*/", "packages/*/"],
     },
   },
 };
