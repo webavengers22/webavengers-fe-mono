a3d39ba4e97a19352b7cdee35f21125fb986c028
HwangJuwon <hjuwon0605@gmail.com>
2023-07-24 20:11:42 +0900

    test : git push commit test

---
+++ apps/web/src/pages/login/index.tsx
@@ -0,0 +1,15 @@
+// pages/login/index.tsx
+
+import { FC } from 'react';
+
+const LoginPage: FC = () => {
+  console.log('test');
+  return (
+    <div>
+      {/* Login page contents here */}
+      <div>로그인</div>
+    </div>
+  );
+};
+
+export default LoginPage;
