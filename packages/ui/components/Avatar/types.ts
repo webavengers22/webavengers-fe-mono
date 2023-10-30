b89c0ab42f6a8f7025370ca7c4c91b09286a4864
짱유나 <yajung0203@naver.com>
2023-10-30 21:07:31 +0900

    feat : packages/ui Avatar ui 컴포넌트 추가

---
+++ packages/ui/components/Avatar/types.ts
@@ -0,0 +1,5 @@
+export interface AvatarProps {
+  nickname: string;
+  imageSrc: string;
+  link: string;
+}
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -5,3 +5,4 @@ export * from './components/IconButton';
 export * from './components/ImageCard';
 export * from './components/ScrollArea';
 export * from './components/Select';
+export * from './components/Avatar';
