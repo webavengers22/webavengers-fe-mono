ce85e7c246e45484a830c1826b741163669fce1b
짱유나 <yajung0203@naver.com>
2023-10-30 22:29:56 +0900

    feat : Empty 컴포넌트 추가

---
+++ packages/ui/components/Empty/theme.ts
@@ -0,0 +1,11 @@
+import tw from 'twin.macro';
+
+export const theme = {
+  container: [
+    tw`w-full`,
+    tw`flex flex-col items-center justify-center gap-3`,
+    tw`my-6`,
+    tw`text-black`,
+  ],
+  text: tw`select-none text-bd1-b`,
+};
