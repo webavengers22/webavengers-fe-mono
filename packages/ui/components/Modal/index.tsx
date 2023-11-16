941005ab2b6ec06f9fcabd961d9e1aa3eeecc5b5
정윤아 <zzangouona@estsecurity.com>
2023-11-17 08:56:41 +0900

    remove : 안 쓰는 import 삭제

--- packages/ui/components/Modal/index.tsx
+++ packages/ui/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes, forwardRef } from 'react';
+import { HTMLAttributes, forwardRef } from 'react';
 import { Icon } from 'icons';
 import { Button } from '../Button';
 import { theme } from './theme';
